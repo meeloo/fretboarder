7caf702541cf843097b11011dfc3572486d66dc2
Sébastien Métrot <smetrot@apple.com>
2020-04-08 12:08:25 +0200

    started to add the UI

--- sources/Fretboarder.cpp
+++ sources/Fretboarder.cpp
@@ -2,25 +2,19 @@
 #include <Fusion/FusionAll.h>
 #include "Fretboard.hpp"
 #include <iostream>
+#include <sstream>
 
 using namespace adsk::core;
 using namespace adsk::fusion;
 using namespace fretboarder;
 
 Ptr<Application> app;
+Ptr<UserInterface> ui;
+
 
 Ptr<Point3D> create_point(Point point) {
     return Point3D::create(0.1 * point.x, 0.1 * point.y, 0);
 }
-
-//def string(value):
-//    return "{} cm".format(quantize(value))
-//
-
-//def create_line(sketch_lines, vector):
-//    sketch_lines.addByTwoPoints(
-//        create_point(vector.point1), create_point(vector.point2)
-//    )
 
 void create_closed_polygon(const Ptr<SketchLines>& sketch_lines, const Quad& shape) {
     for (int index = 1; index < 4;  index++) {
@@ -45,12 +39,7 @@ Ptr<Sketch> create_radius_circle(const Ptr<Component>& component, const Ptr<Cons
 }
 
 
-extern "C" XI_EXPORT bool run(const char* context)
-{
-    app = Application::get();
-    if (!app)
-        return false;
-
+bool createFretboard(const fretboarder::Instrument& instrument) {
     Ptr<Document> doc = app->activeDocument();
     if (!doc)
         return false;
@@ -68,7 +57,6 @@ extern "C" XI_EXPORT bool run(const char* context)
     if(!rootComp)
         return false;
 
-    fretboarder::Instrument instrument;
     fretboarder::Fretboard fretboard(instrument);
     
     // Create a new sketch on the xy plane.
@@ -90,24 +78,6 @@ extern "C" XI_EXPORT bool run(const char* context)
     if(!sketchLines)
         return false;
 
-    if (0) {
-        std::cout << "Fretboarder online" << std::endl;
-        for (int i = 0; i < instrument.number_of_frets; i++) {
-            auto fret = fretboard.fret_slots().at(i);
-            std::cout << "\tfret " << i << std::endl;
-    //        app->userInterface()->messageBox("fret");
-
-            Ptr<SketchLine> line = sketchLines->addByTwoPoints(Point3D::create(fret.point1.x * 0.1, fret.point1.y * 0.1, 0), Point3D::create(fret.point2.x * 0.1, fret.point2.y * 0.1, 0));
-        }
-
-        auto strings = fretboard.strings();
-        for (int i = 0; i < strings.size(); i++) {
-            auto s = strings.at(i);
-            sketchLines->addByTwoPoints(Point3D::create(s.x_at_start() * 0.1, s.y_at_start() * 0.1, 0), Point3D::create(s.x_at_bridge() * 0.1, s.y_at_bridge() * 0.1, 0));
-        }
-        std::cout << "Fretboarder done" << std::endl;
-    }
-    
     // create Fretboard component
     auto occurrence = design->rootComponent()->occurrences()->addNewComponent(Matrix3D::create());
     auto component = occurrence->component();
@@ -283,7 +253,244 @@ extern "C" XI_EXPORT bool run(const char* context)
     auto cut_input = component->features()->combineFeatures()->createInput(main_body, collection);
     cut_input->operation(FeatureOperations::CutFeatureOperation);
     component->features()->combineFeatures()->add(cut_input);
-            
+
+    return true;
+}
+
+
+// Adds a new row to the table.
+static void addRowToTable(Ptr<TableCommandInput> tableInput)
+{
+    if (!tableInput)
+        return;
+
+    // Define a unique id for each row.
+    static int rowNumber = 0;
+    std::stringstream rowId;
+    rowId << rowNumber;
+
+    // Get the CommandInputs object associated with the parent command.
+    Ptr<CommandInputs> cmdInputs = tableInput->commandInputs();
+
+    // Create three new command inputs.
+    Ptr<CommandInput> childTableValueInput = cmdInputs->addValueInput("TableInput_value" + rowId.str(), "Value", "cm", ValueInput::createByReal(rowNumber));
+    Ptr<CommandInput> childTableStringInput = cmdInputs->addStringValueInput("TableInput_string" + rowId.str(), "String", rowId.str());
+    Ptr<CommandInput> childTableSpinnerInput = cmdInputs->addIntegerSpinnerCommandInput("spinnerInt" + rowId.str(), "Integer Spinner", 0, 100, 2, rowNumber);
+
+    // Add the inputs to the table.
+    int row = tableInput->rowCount();
+    tableInput->addCommandInput(childTableValueInput, row, 0);
+    tableInput->addCommandInput(childTableStringInput, row, 1);
+    tableInput->addCommandInput(childTableSpinnerInput, row, 2);
+
+    rowNumber = rowNumber + 1;
+}
+
+// InputChange event handler.
+class OnInputChangedEventHander : public adsk::core::InputChangedEventHandler
+{
+public:
+    void notify(const Ptr<InputChangedEventArgs>& eventArgs) override
+    {
+        Ptr<CommandInputs> inputs = eventArgs->inputs();
+        if (!inputs)
+            return;
+
+        Ptr<CommandInput> cmdInput = eventArgs->input();
+        if (!cmdInput)
+            return;
+
+        Ptr<TableCommandInput> tableInput = inputs->itemById("table");
+        if (!tableInput)
+            return;
+
+        if (cmdInput->id() == "tableAdd") {
+            addRowToTable(tableInput);
+        }
+        else if (cmdInput->id() == "tableDelete") {
+            if (tableInput->selectedRow() == -1) {
+                ui->messageBox("Select one row to delete.");
+            }
+            else {
+                tableInput->deleteRow(tableInput->selectedRow());
+            }
+        }
+    }
+};
+
+class OnExecuteEventHander : public adsk::core::CommandEventHandler
+{
+public:
+    void notify(const Ptr<CommandEventArgs>& eventArgs) override
+    {
+        ui->messageBox("execute!");
+        fretboarder::Instrument instrument;
+        createFretboard(instrument);
+    }
+};
+
+// CommandDestroyed event handler
+class OnDestroyEventHandler : public adsk::core::CommandEventHandler
+{
+public:
+    void notify(const Ptr<CommandEventArgs>& eventArgs) override
+    {
+        adsk::terminate();
+    }
+};
+
+// CommandCreated event handler.
+class CommandCreatedEventHandler : public adsk::core::CommandCreatedEventHandler
+{
+public:
+    void notify(const Ptr<CommandCreatedEventArgs>& eventArgs) override
+    {
+        if (eventArgs)
+        {
+            // Get the command that was created.
+            Ptr<Command> command = eventArgs->command();
+            if (command)
+            {
+                // Connect to the command destroyed event.
+                Ptr<CommandEvent> onDestroy = command->destroy();
+                if (!onDestroy)
+                    return;
+                bool isOk = onDestroy->add(&onDestroyHandler);
+                if (!isOk)
+                    return;
+
+                // Connect to the command execute event.
+                Ptr<CommandEvent> onExecute = command->execute();
+                if (!onExecute)
+                    return;
+                isOk = onExecute->add(&onExecuteHandler);
+                if (!isOk)
+                    return;
+
+
+                // Connect to the input changed event.
+                Ptr<InputChangedEvent> onInputChanged = command->inputChanged();
+                if (!onInputChanged)
+                    return;
+                isOk = onInputChanged->add(&onInputChangedHandler);
+                if (!isOk)
+                    return;
+
+                // Get the CommandInputs collection associated with the command.
+                Ptr<CommandInputs> inputs = command->commandInputs();
+                if (!inputs)
+                    return;
+
+                
+                auto number_of_strings_slider = inputs->addIntegerSliderCommandInput("number_of_strings", "Number of strings", 1, 20);
+                number_of_strings_slider->valueOne(6);
+                inputs->addFloatSpinnerCommandInput("scale_length_left", "Left scale length", "mm", 50, 10000, 0.1, 647.7);
+                inputs->addFloatSpinnerCommandInput("scale_length_right", "Right scale length", "mm", 50, 10000, 0.1, 635.7);
+
+                inputs->addFloatSpinnerCommandInput("perpendicular_fret_index", "Perpendicular Fret", "", 0, 36, 0.1, 0.0);
+
+                inputs->addFloatSpinnerCommandInput("string_spacing_at_nut", "String spacing at nut", "mm", 2, 200, 0.1, 43.0);
+                inputs->addFloatSpinnerCommandInput("string_spacing_at_bridge", "String spacing at bridge", "mm", 2, 200, 0.1, 70.0);
+
+                inputs->addBoolValueInput("has_zero_fret", "Zero fret", true, "", true);
+                inputs->addFloatSpinnerCommandInput("nut_to_zero_fret_offset", "Distance from nut to zero fret", "mm", 0, 200, 0.1, 3.0);
+                inputs->addFloatSpinnerCommandInput("space_before_nut", "Space before nut", "mm", 0, 100, 0.1, 12.0);
+                inputs->addFloatSpinnerCommandInput("nut_thickness", "Nut thickness", "mm", 0, 100, 0.1, 4.0);
+                inputs->addFloatSpinnerCommandInput("nut_height_under", "Nut depth", "mm", 0, 100, 0.1, 3.0);
+
+                inputs->addFloatSpinnerCommandInput("radius_at_nut", "Fretboard radius at nut", "", 0, 10000, 0.1, 254.0);
+                inputs->addFloatSpinnerCommandInput("radius_at_last_fret", "Fretboard radius at last fret", "", 0, 10000, 0.1, 508.0);
+                inputs->addFloatSpinnerCommandInput("fretboard_thickness", "Fretboard thickness", "", 0, 100, 0.1, 7.0);
+
+                auto number_of_frets_slider = inputs->addIntegerSliderCommandInput("number_of_frets", "Number of frets", 0, 36);
+                number_of_frets_slider->valueOne(24);
+                inputs->addFloatSpinnerCommandInput("overhang", "Fret overhang", "mm", 0, 50, 0.1, 3.0);
+                inputs->addFloatSpinnerCommandInput("hidden_tang_length", "Hidden tang length", "mm", 0, 50, 0.1, 2.0);
+                inputs->addFloatSpinnerCommandInput("fret_slots_width", "Fret slots width", "mm", 0, 2, 0.1, 0.6);
+                inputs->addFloatSpinnerCommandInput("fret_slots_height", "Fret slots height", "mm", 0, 10, 0.1, 1.5);
+                inputs->addFloatSpinnerCommandInput("last_fret_cut_offset", "Last fret cut offset", "mm", 0, 10, 0.1, 0);
+
+
+                
+#if 0
+                // Create a read only textbox input.
+//                inputs->addTextBoxCommandInput("readonly_textBox", "Text Box 1", "This is an example of a read-only text box.", 2, true);
+
+                // Create a message that spans the entire width of the dialog by leaving out the "name" argument.
+//                std::string message = "<div align=\"center\">A \"full width\" message using <a href=\"http:fusion360.autodesk.com\">html.</a></div>";
+//                inputs->addTextBoxCommandInput("fullWidth_textBox", "", message, 1, true);
+
+                // Create value input.
+                inputs->addValueInput("value", "Value", "cm", ValueInput::createByReal(0.0));
+
+                // Create bool value input with checkbox style.
+                inputs->addBoolValueInput("checkbox", "Checkbox", true, "", false);
+
+                // Create bool value input with button style that can be clicked.
+                //inputs->addBoolValueInput("buttonClick", "Click Button", false, "resources/button", true);
+
+                // Create float slider input with two sliders and visible texts
+                Ptr<FloatSliderCommandInput> floatSlider3 = inputs->addFloatSliderCommandInput("floatSlider3", "Float Slider 3", "", 0, 50.0, false);
+                if (!floatSlider3)
+                    return;
+                floatSlider3->setText("Min", "Max");
+
+                // Create integer slider input with one slider
+                inputs->addIntegerSliderCommandInput("intSlider", "Integer Slider", 0, 10);
+
+                // Create float spinner input.
+                inputs->addFloatSpinnerCommandInput("spinnerFloat", "Float Spinner", "cm", 0.2, 9.0, 2.2, 1);
+
+                // Create integer spinner input.
+                inputs->addIntegerSpinnerCommandInput("spinnerInt", "Integer Spinner", 2, 9, 2, 3);
+#endif
+            }
+        }
+    }
+private:
+    OnExecuteEventHander onExecuteHandler;
+    OnDestroyEventHandler onDestroyHandler;
+    OnInputChangedEventHander onInputChangedHandler;
+} _cmdCreatedHandler;
+
+
+
+extern "C" XI_EXPORT bool run(const char* context)
+{
+    app = Application::get();
+    if (!app)
+        return false;
+    
+    ui = app->userInterface();
+    if (!ui)
+        return false;
+
+    // Create the command definition.
+    Ptr<CommandDefinitions> commandDefinitions = ui->commandDefinitions();
+    if (!commandDefinitions)
+        return nullptr;
+
+    // Get the existing command definition or create it if it doesn't already exist.
+    Ptr<CommandDefinition> cmdDef = commandDefinitions->itemById("cmdInputsSample");
+    if (!cmdDef)
+    {
+        cmdDef = commandDefinitions->addButtonDefinition("cmdInputsSample",
+                                                          "Command Inputs Sample",
+                                                          "Sample to demonstrate various command inputs.");
+    }
+
+    // Connect to the command created event.
+    Ptr<CommandCreatedEvent> commandCreatedEvent = cmdDef->commandCreated();
+    if (!commandCreatedEvent)
+        return false;
+    commandCreatedEvent->add(&_cmdCreatedHandler);
+
+    // Execute the command definition.
+    cmdDef->execute();
+
+    // Prevent this module from being terminated when the script returns, because we are waiting for event handlers to fire.
+    adsk::autoTerminate(false);
+
     return true;
 }
 
